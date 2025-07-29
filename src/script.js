aee34979c60373895818c1b5d4dd2c666579b7af
mohamedaaqib3005 <mohamedaaqib3005@gmail.com>
2025-07-29 23:53:13 +0530

    grid created

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const grid = document.getElementById("grid");
 
-for (i=0;i<100;i++){
- const cell = document.getElementById("span")
- grid.appendChild(cell);
+for (i = 0; i < 100; i++) {
+    const cell = document.createElement("span");
+    grid.appendChild(cell);
 }
