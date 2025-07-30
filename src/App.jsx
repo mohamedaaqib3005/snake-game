2c0b5d873d38430dd9ed5ec58c6c16d981b95f5e
mohamedaaqib3005 <mohamedaaqib3005@gmail.com>
2025-07-30 14:55:59 +0530

    creating jsx for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import React from 'react';
+import './App.css';
 
-import './App.css'
+const stateGrid = [
+  [0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+  [1, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+  [0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
+  [1, 0, 1, 1, 0, 0, 0, 1, 0, 1],
+  [0, 1, 0, 1, 1, 0, 1, 0, 1, 0],
+  [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+  [0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+  [1, 0, 0, 1, 1, 0, 0, 1, 1, 1],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+];
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <div id="grid">
+      {stateGrid.map((row, rowIndex) =>
+        row.map((state, colIndex) => (
+          <div
+            key={`${rowIndex}-${colIndex}`}
+            className={`cell ${state === 1 ? 'active' : ''}`}
+          ></div>
+        ))
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
