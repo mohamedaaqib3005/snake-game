ea73207dd80f9a7c55852fa1c25fed91e21910cc
mohamedaaqib3005 <mohamedaaqib3005@gmail.com>
2025-08-01 01:26:16 +0530

    snake movement

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import React,{ useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const GRID_SIZE = 10;
 
 function App() {
-  const [snake] = useState([
+  const [snake, setSnake] = useState([
     [5, 4], // head
     [5, 3],
     [5, 2], // tail
   ]);
+  const DIRECTIONS = {
+    ArrowUp: [-1, 0],
+    ArrowDown: [1, 0],
+    ArrowLeft: [0, -1],
+    ArrowRight: [0, 1],
+  };
+
+  const direction = 'ArrowRight';
 
   const isSnakeCell = (row, col) =>
     snake.some(([r, c]) => r === row && c === col);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSnake((prev) => {
+        const [headRow, headCol] = prev[0];
+        const [dRow, dCol] = DIRECTIONS[direction];
+        let newRow = headRow + dRow;
+        let newCol = headCol + dCol;
+        const newHead = [newRow, newCol];
+        return [newHead, ...prev.slice(0, -1)];
+      });
+    }, 300);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div id="grid">
